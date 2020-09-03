c1e137581736287d4ec43cb5e1be31eb466846c7
Kartik Bhanderi <18bce024@nirmauni.ac.in>
2020-09-03 18:13:15 +0530

    express session part 1

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -28,14 +31,20 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
-
+//app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name : 'session-id',
+  secret : '12345-67890-09876-54321',
+  saveUnitialized : false,
+  resave : false,
+  store : new FileStore()
+}));  
 //authorization
 
 function auth(req , res , next){
-  console.log(req.signedCookies);
+  console.log(req.session);
   
-  if(!req.signedCookies.user)
+  if(!req.session.user)
   {  
     var authHeader = req.headers.authorization;
     if(!authHeader){
@@ -51,7 +60,7 @@ function auth(req , res , next){
   
     if(userName == "kartik" && password === "password")
     {
-      res.cookie('user','kartik' , { signed : true })
+      req.session.user = 'kartik';
       next();
     }
     else
@@ -64,7 +73,7 @@ function auth(req , res , next){
   }
   else
   {
-    if(req.signedCookies.user === 'kartik')
+    if(req.session.user === 'kartik')
     {
       console.log('Old user');
       next();
